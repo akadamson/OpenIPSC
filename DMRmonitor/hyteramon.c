23041d0c18064a5729f628265c2a4300c35de443
David Kierzkowski <davidkierz@gmail.com>
2012-12-04 10:45:28 -0500

    test commit #2

--- DMRmonitor/hyteramon.c
+++ DMRmonitor/hyteramon.c
@@ -15,7 +15,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
@@ -28,7 +27,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
 #include<netinet/ip.h>
 #include<getopt.h>
 #include<time.h>
-
 #define NUMSLOTS 2
 #define SLOT1 4369
 #define SLOT2 8738
