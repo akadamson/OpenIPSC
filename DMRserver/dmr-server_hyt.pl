bbf2d98ff7e70d8265ea8caf7e4f4c6c1333d5ed
root <root@wcarlson-Aspire-5517.(none)>
2012-12-06 15:13:02 -0500

    hytera server side

---
+++ DMRserver/dmr-server_hyt.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+#
+#     DAVID KIERZKOWSKI, KD8EYF
+#     13287 Teak Ct.
+#     Sterling Heights, MI 48312
+#
+use IO::Socket::INET;                   #Load Network module
+use DBI;                                #load db module
+my ($socket,$rdata);
+my ($peeraddress,$peerport);
+$err = 0;
+while($Frame = <STDIN>) {
+        $SqlConn ||= DBI->connect("DBI:mysql:database=dmrdb:host=localhost", "dmrprocess", "dmrprocess") or die "Can't connect to database: $DBI::errstr\n";  #connect to db
+	chomp $Frame;
+	($PacketType, $Date,$Time,$RepeaterIP,$SourceNet,$Status,$Slot, $SourceID, $DestinationID, $Calltype, $DestinationType) = split(/ /,$Frame);       #Get The timestamp network and type of packet for pre processing
+        $DateTime = $Date . " " . $Time;                                #assemble timestamp
+	if ($Status == 1) {
+		$Query = "INSERT INTO `dmrdb`.`UserLog` (`StartTime`, `EndTime`, `SourceNet`, `TimeSlot`, `RepeaterID`, `DmrID`, `DestinationID` ) 
+                                       VALUES('$DateTime','0000-00-00 00:00:00','$SourceNet', '$Slot','$RepeaterIP', '$SourceID', '$DestinationID')";
+	} elsif ($Status == 0) {
+		$Query = "UPDATE `dmrdb`.`UserLog` (`EndTime`) VALUES('$DateTime')";
+	};
+        $Statement = $SqlConn->prepare($Query);
+        $Statement->execute();
+}
+$dbh->close;
+exit;
+
